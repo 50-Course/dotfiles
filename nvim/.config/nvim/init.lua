de1d55fb00abc65c17965e9342b8170daa07ff9e
50-Course <erioluwaadeodu@my.uopeople.edu>
2023-08-31 09:47:31 +0100

    setup boiler plate

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,25 @@
 -- [[ init.lua ]] --
 
+-- Gives vim the superpowers of efficient
+-- modal editing (Sets the local leader and leader key to space)
 vim.g.leader = ' '
-vim.g.localleader = ','
+vim.g.localleader = ' '
 
-vim.api.set_nvim_keymap('', '<Space>', '<Nop>)
+-- Make spacebar non-op when used by itself
+vim.keymap.set('n', '<Space>', '<Nop>')
 
+print 'Hello, @codemage'
 
+-- TODO: [[
+--  Setup Treesitter
+--  Setup LsP configuration and Mason
+--  Add Git integration with vim fugitive
+--  Test runner with neo-test
+--  Co-Pilot with git-copilot
+--  ToggleTerm, Trouble, and Vim-tmux-navigator
+--  Dart integration with Fluttertools
+--  Plugin Management with Packer
+--  Undo management with UndoTree
+--  Formatting with null-ls
+--  Debugging with nvim-dap and nvim-dap-ui
+--  ]]
