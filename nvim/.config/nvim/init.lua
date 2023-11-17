6e3ba69426278c70284e679be7d17c70901ec63c
50-Course <erioluwaadeodu@my.uopeople.edu>
2023-11-17 18:44:12 +0100

    Remove `netrw` anonying banner

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,13 +5,19 @@
 --- License: MIT License
 ------------------------------------------
 
-is_linux = vim.uv.os_uname().sys_name == "linux"
-is_macos = vim.uv.os_uname().sys_name == "darwin"
+local is_linux = vim.uv.os_uname().sys_name == "linux"
+local is_macos = vim.uv.os_uname().sys_name == "darwin"
 
 if is_macos then
-    return "ERROR: Vim Conf not yet implemented"
+    print("ERROR: Vim Conf not yet implemented")
+elseif is_linux then
+    print("ERROR: Vim Conf not yet implemented")
+else
+    print("ERROR: Vim Conf not yet implemented")
 end
 
+-- TODO: move this into the settings file
+vim.g.netrw_banner = 0 -- Disable netrw banner (annoying)
 require("codemage.settings")
 require("codemage.plugins")
 require("codemage.keymaps")
